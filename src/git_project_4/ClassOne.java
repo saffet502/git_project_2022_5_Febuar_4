3aec598ff1752802cbfdcf62a131e72d6f4a9313
Ibrahim <83337381+saffet502@users.noreply.github.com>
2022-02-05 18:44:41 +0100

    Update ClassOne.java
    Erste Commit von remote feature Branch

--- src/git_project_4/ClassOne.java
+++ src/git_project_4/ClassOne.java
@@ -5,6 +5,7 @@ public class ClassOne {
 	public static void main(String[] args) {
 	
 	System.out.println("Erste Commit von Local Master");
+	System.out.println("Erste Commit von Local Master");
 
 	}
 
